b76bf69ebbda1a2041988cb56108d1a3d321e1b5
Anand Gupta <anandgupta@197NODMB21115.local>
2018-07-16 11:41:21 +0530

    Handled css module loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var generator = require("babel-generator").default;
+
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-exports.default = function(babel) {
+exports.default = function (babel) {
     var t = babel.types;
 
     function parseAsString(expression) {
@@ -17,6 +19,26 @@ exports.default = function(babel) {
             code = code.slice(0, code.length - 1);
         }
         return code;
+    }
+
+    function createPseudoElement(dataAttr, typeAttr, bodyArr, tagName) {
+        var ifNode = t.jSXIdentifier(tagName);
+        var attrArr = [];
+        var pseudoDataAttr = null;
+        if (dataAttr) {
+            pseudoDataAttr = t.jSXAttribute(t.jSXIdentifier('data'), t.stringLiteral(dataAttr));
+            attrArr.push(pseudoDataAttr);
+        }
+        var pseudoTypeAttr = null;
+        if (typeAttr) {
+            pseudoTypeAttr = t.jSXAttribute(t.jSXIdentifier('type'), t.stringLiteral(typeAttr));
+            attrArr.push(pseudoTypeAttr);
+        }
+        var pseudoOpen = t.jSXOpeningElement(ifNode, attrArr, false);
+        var pseudoClose = t.jSXClosingElement(t.jSXIdentifier(tagName));
+        var pseudoElem = t.jSXElement(pseudoOpen, pseudoClose, bodyArr);
+        pseudoElem.selfClosing = false;
+        return pseudoElem;
     }
 
     function processLogicalExp(path, lazyTags) {
@@ -39,21 +61,10 @@ exports.default = function(babel) {
         /**
          * Creating pseudo html element and replacing with logical expression.
          */
-        var pseudo = t.jSXIdentifier('PLUGIN-CONDITION');
-        var pseudoDataAttr = t.jSXAttribute(t.jSXIdentifier('data'), t.stringLiteral(stringexp));
-        var pseudoTypeAttr = t.jSXAttribute(t.jSXIdentifier('type'), t.stringLiteral(logicType));
-        var pseudoOpen = t.jSXOpeningElement(pseudo, [pseudoDataAttr, pseudoTypeAttr], false);
-        var pseudoClose = t.jSXClosingElement(t.jSXIdentifier('PLUGIN-CONDITION'));
-        var pseudoEle = t.jSXElement(pseudoOpen, pseudoClose, [body]);
-        path.replaceWith(pseudoEle);
+        path.replaceWith(createPseudoElement(stringexp, logicType, [body], 'PLUGIN-CONDITION'));
     }
-/**
- * 
- *
- * @param {*} path
- * @param {*} state
- */
-function processCallExp(path, state) {
+
+    function processCallExp(path, state) {
         var exp = path.node.expression;
 
         /**
@@ -66,43 +77,47 @@ function processCallExp(path, state) {
         if (t.isCallExpression(exp) && exp.callee.property.name == 'map') {
             var object = exp.callee.object;
             var propPath = [];
+            var type = null;
+            var bodyElem = null;
             while (t.isMemberExpression(object)) {
                 propPath.unshift(object.property.name);
                 object = object.object;
             }
             //object should be an identifier if not a member exp
             propPath.unshift(object.name);
-            var type = exp.arguments[0].params[0].name;
-            var body = exp.arguments[0].body.body[0].argument;
+            if (Array.isArray(exp.arguments) && Array.isArray(exp.arguments[0].params)) {
+                type = exp.arguments[0].params[0].name;
+            }
+            if (Array.isArray(exp.arguments) && exp.arguments[0].body && Array.isArray(exp.arguments[0].body.body)) {
+                var element = exp.arguments[0].body.body[0];
+                if (element.argument) {
+                    bodyElem = element.argument;
+                } else if (element.type = "IfStatement") {
+                    bodyElem = createPseudoElement(parseAsString(element.test), element.test.operator, [element.consequent.body[0].argument], 'PLUGIN-CONDITION');
+                }
+            }
             var prop = propPath.join('.');
-            /**
-            * Creating pseudo html element and replacing with logical expression.
-            */
-            var pseudo = t.jSXIdentifier('PLUGIN-LOOP');
-            var pseudoDataAttr = t.jSXAttribute(t.jSXIdentifier('data'), t.stringLiteral(prop));
-            var pseudoTypeAttr = t.jSXAttribute(t.jSXIdentifier('type'), t.stringLiteral(type));
-            var pseudoOpen = t.jSXOpeningElement(pseudo, [pseudoDataAttr, pseudoTypeAttr], false);
-            var pseudoClose = t.jSXClosingElement(t.jSXIdentifier('PLUGIN-LOOP'));
-            var pseudoEle = t.jSXElement(pseudoOpen, pseudoClose, [body]);
+
+            var bodyArray = [];
+            if (bodyElem) {
+                bodyArray.push(bodyElem);
+            }
+            // create pseudo element
+            var pseudoEle = createPseudoElement(prop, type, bodyArray, 'PLUGIN-LOOP');
             path.replaceWith(pseudoEle);
         } else if (t.isCallExpression(exp) && state.classMethods.hasOwnProperty(exp.callee.property.name)) {
-
-            state.exp.splice( state.exp.indexOf(exp), 1 );
-
+            state.exp.splice(state.exp.indexOf(exp), 1);
             const renderElem = genericTraverse(state.classMethods[exp.callee.property.name], state);
-
-            if(path && path.node && path.node.expression && path.node.expression.callee) {
-
+            if (path && path.node && path.node.expression && path.node.expression.callee) {
                 /**
                  * If needed, Attributes can be added for function reference.
                  */
                 // renderElem.openingElement.attributes.push(t.jSXAttribute(t.jSXIdentifier('func-ref'), t.stringLiteral(path.node.expression.callee.property.name)));
-                
+
                 /**
                  * Recursively replacing function call with JSX. 
                  */
                 path.replaceWith(renderElem);
-
             }
         }
     }
@@ -117,7 +132,7 @@ function processCallExp(path, state) {
      */
     function genericTraverse(path, state) {
 
-        var returnNode = path.node.body.body.filter(function(obj) {
+        var returnNode = path.node.body.body.filter(function (obj) {
             return t.isReturnStatement(obj);
         });
         var renderReturn = returnNode[0].argument;
@@ -138,7 +153,7 @@ function processCallExp(path, state) {
         });
         path.stop();
 
-        state.exp.map(function(path) {
+        state.exp.map(function (path) {
             if (t.isLogicalExpression(path.node.expression)) {
                 processLogicalExp(path, state.opts.lazyTags);
             } else if (t.isCallExpression(path.node.expression)) {
@@ -148,26 +163,6 @@ function processCallExp(path, state) {
 
         return renderReturn;
     }
-
-    /**
-     * Parse class methods
-     *
-     * @param {*} path
-     * @param {*} state
-     */
-    function classParse(path, state) {
-        path.traverse({
-            ClassMethod: function ClassMethod(path) {
-                if (path.node.key.name === 'render') {
-                    const renderReturn = genericTraverse(path, state);
-                    state.mainPlace.replaceWith(t.program([t.expressionStatement(renderReturn)]));
-                } else {
-                    state.classMethods[path.node.key.name] = path;
-                }
-            }
-        });
-    }
-
 
 
     return {
@@ -212,10 +207,56 @@ function processCallExp(path, state) {
                         }
                     }
                 }
-
-                path.remove();
+                // path.remove();
             },
-            Class: classParse
+            Class: function (path, state) {
+                path.traverse({
+                    ClassMethod: function ClassMethod(path) {
+                        state.classMethods[path.node.key.name] = path;
+                    }
+                });
+
+                Object.keys(state.classMethods).forEach(element => {
+                    if (element === 'render') {
+                        const renderReturn = genericTraverse(state.classMethods[element], state);
+                        state.mainPlace.replaceWith(t.Program([t.expressionStatement(renderReturn)]));
+                    }
+                });
+            },
+            VariableDeclaration: function (path, state) {
+                if (path.node.declarations[0].id.name === 'style') {
+                    var ast = {
+                        type: 'Program',
+                        body: [path.node]
+                    };
+                    var styleString = generator(ast, {}, "").code;
+                    var styleObj = JSON.parse(styleString.substring(styleString.indexOf("{"), styleString.indexOf("}") + 1));
+                    state.style = styleObj;
+                }
+            },
+            JSXIdentifier: function (path) {
+                if (path.node.name === 'className') {
+                    path.node.name = 'class';
+                }
+            },
+            JSXExpressionContainer: function (path, state) {
+                if (path.node.expression.type === 'TemplateLiteral') {
+                    var stringLiteral = '';
+                    path.node.expression.expressions.forEach(element => {
+                        if (element.type === 'MemberExpression') {
+                            if (element.object.name === 'style') {
+                                stringLiteral += ' ' + state.style[element.property.value];
+                            }
+                        }
+                    });
+                    path.node.expression.quasis.forEach(element => {
+                        stringLiteral += element.value.cooked;
+                    });
+
+                    path.replaceWith(t.stringLiteral(stringLiteral));
+                }
+
+            }
         }
     };
 };
